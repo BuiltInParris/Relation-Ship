2e8ea5a94554f30ad67786aba22770042d2decb2
BuiltInParris <BuiltInParris@users.noreply.github.com>
2020-02-01 00:02:28 -0500

    Adding models

---
+++ Assets/Scripts/Data Models/Device.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device : MonoBehaviour
+{
+    bool isDamaged;
+
+    double timeRepaired;
+
+    int variableAmountOfTime;
+
+    double lastRepaired;
+
+    System.Random rnd;
+
+    double randomTimeTillNextDamage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDamaged = true;
+        timeRepaired = 10;
+        variableAmountOfTime = 3;
+        lastRepaired = 0;
+        randomTimeTillNextDamage = rnd.Next(0, variableAmountOfTime * 2) - variableAmountOfTime;
+        rnd = new System.Random();
+    }
+
+    // Update is called once per physics loop
+    void FixedUpdate()
+    {
+        if(!isDamaged){
+            lastRepaired = lastRepaired + 0.02;
+            if(lastRepaired > timeRepaired + randomTimeTillNextDamage){
+                isDamaged = true;
+                randomTimeTillNextDamage = rnd.Next(0, variableAmountOfTime * 2) - variableAmountOfTime;
+                lastRepaired = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Data Models/Train.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+
+    int numberOfCars;
+    int speed = 1;
+    List<Car> cars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cars = new List<Car>();
+        for (int i = 0; i < numberOfCars; i++){
+            Car car = new Car();
+            car.location = i;
+            cars.Add(car);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
