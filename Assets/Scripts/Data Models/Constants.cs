e2db33200b3b859348617dff73a6528888d02ddb
Spencer Melnick <smelnick97@gmail.com>
2020-02-02 00:25:20 -0500

    Added scrolling background

--- Assets/Scripts/Data Models/Constants.cs
+++ Assets/Scripts/Data Models/Constants.cs
@@ -8,6 +8,6 @@ public static class Constants
     public const int TIME_STUNNED = 3;
     public const int ADDITONAL_CARS = 2;
     public const int DEVICE_POINT_VALUE = 5;
-    public const float PLAYER_CAMERA_MINIMUM_DISTANCE = 9f;
+    public const float PLAYER_CAMERA_MINIMUM_DISTANCE = 20f;
     public const double STUN_COOLDOWN = 3.0;
 }
---
+++ Assets/Scripts/ScrollTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public float scrollSpeed = 10.0f;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector2 offset = spriteRenderer.material.mainTextureOffset;
+        offset.x += scrollSpeed *Time.deltaTime;
+        spriteRenderer.material.mainTextureOffset = offset;
+
+        Debug.Log(spriteRenderer.material.mainTextureOffset.x);
+    }
+}
