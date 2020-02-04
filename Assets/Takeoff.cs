e0b8f24eeefa3d9dd033a17b680ce610153818c3
BuiltInParris <BuiltInParris@users.noreply.github.com>
2020-02-03 19:13:00 -0500

    Adding

--- Assets/Takeoff.cs
+++ Assets/Takeoff.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 
 public class Takeoff : MonoBehaviour
 {
-    public float yVel = 500;
-    public int playerNum;
-
-    Dictionary<int, int> velocities;
+    private float yVel = 0;
     private int players = 0;
-    //1050 is 0 players
-    //1100 is 1 player
-    //1200 is 2 players
-    //1250 is 3 players
-    //1300 is 4 players
 
     // Start is called before the first frame update
     void Start()
     {
-        velocities = new Dictionary<int, int>() { { 0, 1050 }, { 1, 1100 }, { 2, 1200 }, { 3, 1250 }, { 4, 1300 } };
-
         int totalScore = 0;
 
         List<GameState.PlayerState> states = new List<GameState.PlayerState>();
@@ -28,27 +18,13 @@ public class Takeoff : MonoBehaviour
             totalScore += playerState.score;
         }
 
-        int playersLeft = 0;
+        int startVelocity = 1050;
 
-        if(totalScore > 50){
-            playersLeft = 1;
-        } else if (totalScore > 100){
-            playersLeft = 2;
-        } else if (totalScore > 150){
-            playersLeft = 3;
-        } else if(totalScore > 200){
-            playersLeft = 4;
-        }
+        int playersLeft = (int) totalScore / 50;
 
-        GameState.numSurvivors = playersLeft;
-
-        float xForce = 1300;
-
-        if (playersLeft < playerNum)
+        foreach (GameObject trainCar in GameObject.FindGameObjectsWithTag("Train"))
         {
-            xForce = 500;
+            trainCar.GetComponent<Rigidbody2D>().AddForce(new Vector2(startVelocity + playersLeft * 75, yVel));
         }
-
-        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(xForce, yVel));
     }
 }
