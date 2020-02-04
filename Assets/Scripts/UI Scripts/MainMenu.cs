40b01ae4b05f1eb2235e2bce38aca32237b00eff
BuiltInParris <BuiltInParris@users.noreply.github.com>
2020-02-03 22:09:49 -0500

    Repositioning the video to beginning of game

--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -2,27 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
     public Canvas canvas;
-    public VideoPlayer videoPlayer;
 
     public void PlayGame ()
     {
-        // play cutscene
-        canvas.gameObject.SetActive(false);
-        videoPlayer.gameObject.SetActive(true);
+        SceneManager.LoadScene("Scenes/DynamicallyGeneratedLevel");
 
-        // load game
-        videoPlayer.loopPointReached += EndReached;
-
-    }
-
-    void EndReached(UnityEngine.Video.VideoPlayer vp)
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
---
+++ Assets/Scripts/UI Scripts/VideoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoScript : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Scenes/Main Menu");
+    }
+}
