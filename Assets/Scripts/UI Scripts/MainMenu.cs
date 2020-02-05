97b8b23f0d1f91e1dd656f1363adf47b7d2839d8
BuiltInParris <BuiltInParris@users.noreply.github.com>
2020-02-05 07:39:17 -0500

    Dynamically allow more players in menu
    Allows up to as many players as gamepads + 4 keyboard players.

--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Canvas canvas;
 
+    public void Start()
+    {
+        for (var i = 0; i < Gamepad.all.Count; i++)
+        {
+            GetComponentInChildren<Dropdown>().options.Add(new Dropdown.OptionData(NumberToWords(i + 4)));
+        }
+
+    }
+    
     public void PlayGame ()
     {
         SceneManager.LoadScene("Scenes/DynamicallyGeneratedLevel");
@@ -22,5 +34,54 @@ public class MainMenu : MonoBehaviour
     public void SetPlayerCount (int pcIndex)
     {
         GameSettings.playerCount = pcIndex + 2;
+    }
+    
+    public static string NumberToWords(int number)
+    {
+        if (number == 0)
+            return "zero";
+
+        if (number < 0)
+            return "minus " + NumberToWords(Math.Abs(number));
+
+        string words = "";
+
+        if ((number / 1000000) > 0)
+        {
+            words += NumberToWords(number / 1000000) + " million ";
+            number %= 1000000;
+        }
+
+        if ((number / 1000) > 0)
+        {
+            words += NumberToWords(number / 1000) + " thousand ";
+            number %= 1000;
+        }
+
+        if ((number / 100) > 0)
+        {
+            words += NumberToWords(number / 100) + " hundred ";
+            number %= 100;
+        }
+
+        if (number > 0)
+        {
+            if (words != "")
+                words += "and ";
+
+            var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            if (number < 20)
+                words += unitsMap[number];
+            else
+            {
+                words += tensMap[number / 10];
+                if ((number % 10) > 0)
+                    words += "-" + unitsMap[number % 10];
+            }
+        }
+
+        return words;
     }
 }
